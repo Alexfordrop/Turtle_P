46b057af04fd50245696b7197b095588acac8d8d
Alex <mishechkin.aleksei@mail.ru>
2021-04-06 03:53:31 +0300

    -

--- Races.py
+++ Races.py
@@ -29,7 +29,7 @@ def create_turtles(colors):
         racer.shape('turtle')
         racer.left(90)
         racer.penup()
-        # позиция черепашки
+        racer.setpos()
         racer.pendown()
         turtles.append(racer)
 
@@ -45,3 +45,30 @@ random.shuffle(COLORS)
 colors = COLORS[:racers]
 create_turtles(colors)
 
+
+
+##########################
+racer = turtle.Turtle()
+racer2 = turtle.Turtle()
+
+racer.speed(1)
+racer.penup()
+racer.shape("turtle")
+racer.color('cyan')
+racer.forward(100)
+racer.left(90)
+racer.pendown()
+racer.forward(100)
+racer.right(90)
+racer.backward(100)
+
+racer2.speed(5)
+racer2.penup()
+racer2.shape("turtle")
+racer2.color('red')
+racer2.forward(150)
+racer2.left(90)
+racer2.pendown()
+racer2.forward(100)
+racer2.right(90)
+racer2.backward(150)
